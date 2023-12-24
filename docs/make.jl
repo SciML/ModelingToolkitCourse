6cc43b141f43cdc3cff719d8bdd1e261cc47e654
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-12-24 18:50:40 -0500

    Fix up doc build and CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,19 @@
 using Documenter
 
-makedocs(sitename="18.S191")
+pages = [
+    "Home" => "index.md",
+    "lectures/lecture1.md",
+    "lectures/lecture2.md",
+]
+
+makedocs(sitename = "ModelingToolkit Course",
+    authors = "Chris Rackauckas",
+    modules = [],
+    clean = true, doctest = false, linkcheck = true,
+    warnonly = [:missing_docs],
+    format = Documenter.HTML(assets = ["assets/favicon.ico"],
+        canonical = "https://docs.sciml.ai/ModelingToolkitCourse/stable/"),
+    pages = pages)
+
+deploydocs(repo = "github.com/SciML/ModelingToolkitCourse.git";
+    push_preview = true)
