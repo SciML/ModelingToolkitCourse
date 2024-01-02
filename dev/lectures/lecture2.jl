813d6511d2e6864ee72958f78b26fcdb832644a6
Documenter.jl <documenter@juliadocs.github.io>
2024-01-02 00:55:40 +0000

    build based on f9ca82a

--- dev/lectures/lecture2.jl
+++ dev/lectures/lecture2.jl
@@ -1,42 +1,63 @@
 using ModelingToolkit
-using OrdinaryDiffEq
-using ForwardDiff
-
-NEWTON = NLNewton(check_div = false, always_new = true, max_iter = 1000, relax = 4 // 10)
+using DifferentialEquations
+using Symbolics
 
 @parameters t
 D = Differential(t)
 
 # parameters -------
-r₀ = 876 #kg/s
-β = 1.2e9 #Pa
-A = 0.01 #m²
-x₀ = 1.0 #m
-m = 1000 #kg
-g = 9.807 #m/s²
-amp = 1e-3 #m
-f = 10 #Hz
+pars = @parameters begin
+    r₀ = 876 #kg/s
+    β = 1.2e9 #Pa
+    A = 0.01 #m²
+    x₀ = 1.0 #m
+    m = 1000 #kg
+    g = 9.807 #m/s²
+    amp = 1e-3 #m
+    f = 10 #Hz    
+end
+
 dt = 1e-4 #s
 t_end = 0.2 #s
+time = 0:dt:t_end
+
+x_fun(t,amp,f) = amp*sin(2π*t*f) + x₀
+ẋ_fun = build_function(expand_derivatives(D(x_fun(t,amp,f))), t, amp, f; expression=false)
+ẍ_fun = build_function(expand_derivatives(D(ẋ_fun(t,amp,f))), t, amp, f; expression=false)
+
 
 vars = @variables begin
     x(t) = x₀
-    ẋ(t) = 2π*f*amp #m/s
-    ẍ(t) = 0 #m/s²
+    ẋ(t) = ẋ_fun(0.0, amp, f)
+    ẍ(t) = ẍ_fun(0.0, amp, f)
     p(t) = m*g/A #Pa
-    ṁ(t) = 0 #kg/s
-    r(t) = r₀*(1 + p/β)
-    ṙ(t) = 0
+    ṁ(t)
+    r(t)=r₀*(1 + p/β)
+    ṙ(t)=0.0
 end 
 
 # let ------
 V = x*A
 V̇ = ẋ*A
 
-time = 0:dt:t_end
+eqs = [
+    D(x) ~ ẋ 
+    D(ẋ) ~ ẍ
+    D(r) ~ ṙ
 
-x_fun(t) = amp*sin(2π*t*f) + x₀
-# ẋ_fun(t) = ForwardDiff.derivative(x_fun, t)
+    r ~ r₀*(1 + p/β)
+
+    ṁ ~ ṙ*V + r*V̇
+    m*ẍ ~ p*A - m*g
+
+    ṁ ~ ẋ_fun(t,amp,f)*A*r
+]
+
+@named odesys = ODESystem(eqs, t, vars, pars)
+sys = structural_simplify(odesys)
+prob = ODEProblem(sys, [], (0, t_end))
+
+sol_ṁ = solve(prob)
 
 eqs = [
     D(x) ~ ẋ 
@@ -51,11 +72,39 @@ eqs = [
     x ~ x_fun(t)
 ]
 
-@named odesys = ODESystem(eqs, t, vars, [])
+@named odesys = ODESystem(eqs, t, vars, pars)
 sys = structural_simplify(odesys)
-observed(sys)
+prob = ODEProblem(sys, [], (0, t_end))
+sol_x = solve(prob)
 
 
-prob = ODEProblem(sys, [], (0,t_end), [])
+using Plots
+plot(sol_ṁ; idxs=r)
+plot!(time, sol_x(time)[r])
 
-sol_i1 = solve(prob_i1, ImplicitEuler())
+plot(sol_ṁ; idxs=p/1e5)
+plot!(time, sol_x(time)[p]/1e5)
+
+plot(sol_ṁ; idxs=x)
+plot!(time, sol_x(time)[x])
+
+plot(sol_ṁ; idxs=ṁ)
+plot!(time, sol_x(time)[ṁ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
