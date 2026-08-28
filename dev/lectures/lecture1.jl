3530ee3db2e4a0b39139746458c9996dd1b3cfc9
Documenter.jl <documenter@juliadocs.github.io>
2026-08-28 12:41:49 +0000

    build based on c1b2bd4

--- dev/lectures/lecture1.jl
+++ dev/lectures/lecture1.jl
@@ -1,15 +1,15 @@
 using ForwardDiff
 using Plots
 
-d=1
-k=1000
-Δt=1e-3
+d = 1
+k = 1000
+Δt = 1.0e-3
 F = 100
 
 function f(xᵢ, xᵢ₋₁)
 
-    ẋᵢ = (xᵢ - xᵢ₋₁)/Δt
-    lhs = d*ẋᵢ + k*xᵢ^1.5
+    ẋᵢ = (xᵢ - xᵢ₋₁) / Δt
+    lhs = d * ẋᵢ + k * xᵢ^1.5
     rhs = F
 
     return lhs - rhs
@@ -20,148 +20,147 @@ end
 xᵢ₋₁ = 0.0
 xᵢ = xᵢ₋₁ #<-- guess
 g(xᵢ) = f(xᵢ, xᵢ₋₁)
-xᵢ -= g(xᵢ)/ForwardDiff.derivative(g, xᵢ)
-xᵢ -= g(xᵢ)/ForwardDiff.derivative(g, xᵢ)
-xᵢ -= g(xᵢ)/ForwardDiff.derivative(g, xᵢ)
+xᵢ -= g(xᵢ) / ForwardDiff.derivative(g, xᵢ)
+xᵢ -= g(xᵢ) / ForwardDiff.derivative(g, xᵢ)
+xᵢ -= g(xᵢ) / ForwardDiff.derivative(g, xᵢ)
 
 
 # ---------------------------------
 
-tol = 1e-3
+tol = 1.0e-3
 x = zeros(10)
-for i=2:10
-    g(xᵢ) = f(xᵢ, x[i-1])
+for i in 2:10
+    g(xᵢ) = f(xᵢ, x[i - 1])
     Δx = Inf
     while abs(Δx) > tol
-        Δx = g(x[i])/ForwardDiff.derivative(g, x[i]) 
+        Δx = g(x[i]) / ForwardDiff.derivative(g, x[i])
         x[i] -= Δx
     end
 end
 
-plot(x; ylabel="x [m]", xlabel="time step")
+plot(x; ylabel = "x [m]", xlabel = "time step")
 
 # ---------------------------------
 using DifferentialEquations
 
 prob = NonlinearProblem(f, 0.0, 0.0)
-sol=solve(prob, NewtonRaphson(); abstol=tol)
+sol = solve(prob, NewtonRaphson(); abstol = tol)
 
 # ---------------------------------
 
 x = zeros(10)
-for i=2:10
-    prob′ = remake(prob; u0=x[i], p=x[i-1])
-    sol = solve(prob′, NewtonRaphson(); abstol=tol)
+for i in 2:10
+    prob′ = remake(prob; u0 = x[i], p = x[i - 1])
+    sol = solve(prob′, NewtonRaphson(); abstol = tol)
     x[i] = sol[]
 end
-plot(x; ylabel="x [m]", xlabel="time step")
+plot(x; ylabel = "x [m]", xlabel = "time step")
 
 # ---------------------------------
 
-function du_dt(u,p,t)
+function du_dt(u, p, t)
     F, k, d = p
     x = u
-    return (F - k*x^1.5)/d
+    return (F - k * x^1.5) / d
 end
 
 prob = ODEProblem(du_dt, 0.0, (0.0, 0.01), [F, k, d])
 sol = solve(prob)
-plot(sol; xlabel="time [s]", ylabel="x [m]")
+plot(sol; xlabel = "time [s]", ylabel = "x [m]")
 
 # ---------------------------------
-tol = 1e-3
-d=1
-k=1000
+tol = 1.0e-3
+d = 1
+k = 1000
 F = 100
 
-function du_dt1(u,p,t)
+function du_dt1(u, p, t)
     F, k, d = p
     x, ẋ = u
-    
+
     eqs = [
         ẋ                       # D(x) = ẋ
-        (d*ẋ + k*x^1.5) - (F)   #    0 = ( lhs ) - ( rhs )
+        (d * ẋ + k * x^1.5) - (F)   #    0 = ( lhs ) - ( rhs )
     ]
 
     return eqs
 end
 
-fmm = ODEFunction(du_dt1; mass_matrix=[1 0;0 0])
-prob = ODEProblem(fmm, [0.0, F/d], (0.0, 0.01), [F, k, d])
-sol = solve(prob; abstol=tol)
-plot(sol; layout=2)
+fmm = ODEFunction(du_dt1; mass_matrix = [1 0;0 0])
+prob = ODEProblem(fmm, [0.0, F / d], (0.0, 0.01), [F, k, d])
+sol = solve(prob; abstol = tol)
+plot(sol; layout = 2)
 
 
-function du_dt1(du,u,p,t)
+function du_dt1(du, u, p, t)
     F, k, d = p
     x, ẋ, ẍ = u
-    
+
     du[1] = ẋ
     du[2] = ẍ
-    du[3] = (d*ẋ + k*(x^1.5)) - (F)
+    return du[3] = (d * ẋ + k * (x^1.5)) - (F)
 
 end
 
-fmm = ODEFunction(du_dt1; mass_matrix=[1 0 0;0 1 0;0 0 0])
-prob = ODEProblem(fmm, [0.0, F/d, 0.0], (0.0, 0.01), [F, k, d])
+fmm = ODEFunction(du_dt1; mass_matrix = [1 0 0;0 1 0;0 0 0])
+prob = ODEProblem(fmm, [0.0, F / d, 0.0], (0.0, 0.01), [F, k, d])
 
-sol = solve(prob, ImplicitEuler(autodiff=false); abstol=tol)
-sol = solve(prob, ImplicitEuler(); abstol=tol, dt=0.001, adaptive=false)
-sol = solve(prob, Rosenbrock23(); abstol=tol)
+sol = solve(prob, ImplicitEuler(autodiff = false); abstol = tol)
+sol = solve(prob, ImplicitEuler(); abstol = tol, dt = 0.001, adaptive = false)
+sol = solve(prob, Rosenbrock23(); abstol = tol)
 
-plot(sol; layout=3)
+plot(sol; layout = 3)
 
 using ModelingToolkitComponents: SimpleImplicitEuler
-sol = solve(prob, SimpleImplicitEuler(); abstol=tol)
-plot(sol; layout=3)
+sol = solve(prob, SimpleImplicitEuler(); abstol = tol)
+plot(sol; layout = 3)
 
-function du_dt2(u,p,t)
+function du_dt2(u, p, t)
     F, k, d = p
     x, ẋ, ẍ = u
-    
+
     eqs = [
         ẋ                       # D(x) = ẋ
         ẍ                       # D(ẋ) = ẍ
-        (d*ẍ + 1.5*k*(x^0.5)*ẋ) - (0)   #    0 = ( lhs ) - ( rhs )
+        (d * ẍ + 1.5 * k * (x^0.5) * ẋ) - (0)   #    0 = ( lhs ) - ( rhs )
     ]
 
     return eqs
 end
 
-fmm = ODEFunction(du_dt2; mass_matrix=[1 0 0;0 1 0;0 0 0])
-prob = ODEProblem(fmm, [0.0, F/d, 0.0], (0.0, 0.01), [F, k, d])
-sol = solve(prob, ImplicitEuler(); abstol=tol)
+fmm = ODEFunction(du_dt2; mass_matrix = [1 0 0;0 1 0;0 0 0])
+prob = ODEProblem(fmm, [0.0, F / d, 0.0], (0.0, 0.01), [F, k, d])
+sol = solve(prob, ImplicitEuler(); abstol = tol)
 
 
+sol′ = solve(prob, RadauIIA5(); abstol = tol, reltol = 10.0)
 
-sol′ = solve(prob, RadauIIA5(); abstol=tol, reltol=10.0)
+plot(sol′; layout = 3)
 
-plot(sol′ ; layout=3)
+plot(sol; idxs = 1, xlabel = "time [s]", ylabel = "x [m]")
+plot!(sol′; idxs = 1, xlabel = "time [s]", ylabel = "x [m]")
 
-plot(sol; idxs=1, xlabel="time [s]", ylabel="x [m]")
-plot!(sol′; idxs=1, xlabel="time [s]", ylabel="x [m]")
+plot(sol; idxs = 2, xlabel = "time [s]", ylabel = "ẋ [m/s]")
+plot!(sol′; idxs = 2, xlabel = "time [s]", ylabel = "ẋ [m/s]")
 
-plot(sol; idxs=2, xlabel="time [s]", ylabel="ẋ [m/s]")
-plot!(sol′; idxs=2, xlabel="time [s]", ylabel="ẋ [m/s]")
-
-plot(0:1e-5:0.01, x->ForwardDiff.derivative(t->sol(t), x)[2]; xlabel="time [s]", ylabel="ẍ [m/s^2]")
-plot!(sol′; idxs=3, xlabel="time [s]", ylabel="ẍ [m/s^2]")
+plot(0:1.0e-5:0.01, x -> ForwardDiff.derivative(t -> sol(t), x)[2]; xlabel = "time [s]", ylabel = "ẍ [m/s^2]")
+plot!(sol′; idxs = 3, xlabel = "time [s]", ylabel = "ẍ [m/s^2]")
 # ---------------------------------
 
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
-vars = @variables x(t)=0.0 ẋ(t)=F/d ẍ(t)=0.0
+vars = @variables x(t) = 0.0 ẋ(t) = F / d ẍ(t) = 0.0
 eqs = [
     D(x) ~ ẋ
     D(ẋ) ~ ẍ
-    d*ẋ + k*x^1.5 ~ F
+    d * ẋ + k * x^1.5 ~ F
 ]
 @mtkbuild odesys = ODESystem(eqs, t, vars, [])
 sys = structural_simplify(odesys)
 prob = ODEProblem(sys, [], (0.0, 0.01))
-sol = solve(prob; abstol=tol)
-plot(sol; idxs=ẍ, xlabel="time [s]", ylabel="ẍ [m/s^2]")
+sol = solve(prob; abstol = tol)
+plot(sol; idxs = ẍ, xlabel = "time [s]", ylabel = "ẍ [m/s^2]")
 
 ẍ_sol = sol[ẍ]
 t_sol = sol.t
@@ -182,15 +181,15 @@ end
         f(t) = 0
     end
     @components begin
-        port = MechanicalPort(;v=v, f=f)
+        port = MechanicalPort(; v = v, f = f)
     end
     @equations begin
         # connectors
         port.v ~ v
         port.f ~ f
-        
+
         # physics
-        f ~ m*D(v)
+        f ~ m * D(v)
     end
 end
 
@@ -200,18 +199,18 @@ end
     end
     @variables begin
         v(t) = 0
-        f(t) = d*v
+        f(t) = d * v
     end
     @components begin
-        port = MechanicalPort(;v=v, f=f)
+        port = MechanicalPort(; v = v, f = f)
     end
     @equations begin
         # connectors
         port.v ~ v
         port.f ~ f
-        
+
         # physics
-        f ~ d*v
+        f ~ d * v
     end
 end
 
@@ -222,62 +221,38 @@ end
         d
     end
     @components begin
-        mass = Mass(;v,m)
-        damper = Damper(;v,d)
+        mass = Mass(; v, m)
+        damper = Damper(; v, d)
     end
     @equations begin
         connect(mass.port, damper.port)
     end
 end
 
-@mtkbuild sys = System(;v=100, m=5, d=3)
+@mtkbuild sys = System(; v = 100, m = 5, d = 3)
 full_equations(sys)
 
 defs = ModelingToolkit.defaults(sys)
 defs[sys.damper.d]
 
 
-
-
-
-prob1 = ODEProblem(sys, [], (0,10))
+prob1 = ODEProblem(sys, [], (0, 10))
 sol1 = solve(prob1)
-prob2 = remake(prob1; p=[sys.m=>3, sys.d=>4])
+prob2 = remake(prob1; p = [sys.m => 3, sys.d => 4])
 sol2 = solve(prob2)
-plot(sol1; idxs=sys.mass.v)
-plot!(sol2; idxs=sys.mass.v)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plot(sol1; idxs = sys.mass.v)
+plot!(sol2; idxs = sys.mass.v)
 
 
 using DAE2AE
 aesys = dae_to_ae(odesys, Δt, 2)
 sys = no_simplify(aesys)
 prob = ODEProblem(sys, [], (0.0, 0.01))
-sol = solve(prob, ImplicitEuler(nlsolve=NLNewton(check_div=false)); abstol=tol, dt=Δt, adaptive=false, initializealg=NoInit())
-plot(sol; idxs=ẍ, xlabel="time [s]", ylabel="ẍ [m/s^2]")
+sol = solve(prob, ImplicitEuler(nlsolve = NLNewton(check_div = false)); abstol = tol, dt = Δt, adaptive = false, initializealg = NoInit())
+plot(sol; idxs = ẍ, xlabel = "time [s]", ylabel = "ẍ [m/s^2]")
 
 ẍ_sol = sol[ẍ]
 t_sol = sol.t
-
-
 
 
 # -------------------
@@ -288,7 +263,7 @@ t_sol = sol.t
 # -------------------
 # -------------------
 
-Δt = 1e-3
+Δt = 1.0e-3
 
 
 function f(Xᵢ, P)
@@ -300,94 +275,72 @@ function f(Xᵢ, P)
     xᵢ₋₂, ẋᵢ₋₂, ẍᵢ₋₂ = Xᵢ₋₂
 
     eqs = [
-        ( ẋᵢ ) - ( (3*xᵢ - 4*xᵢ₋₁ + xᵢ₋₂)/(2*Δt) )
-        ( ẍᵢ ) - ( (3*ẋᵢ - 4*ẋᵢ₋₁ + ẋᵢ₋₂)/(2*Δt) )
-        ( d*ẋᵢ + k*xᵢ^1.5 ) - ( F )
+        (ẋᵢ) - ((3 * xᵢ - 4 * xᵢ₋₁ + xᵢ₋₂) / (2 * Δt))
+        (ẍᵢ) - ((3 * ẋᵢ - 4 * ẋᵢ₋₁ + ẋᵢ₋₂) / (2 * Δt))
+        (d * ẋᵢ + k * xᵢ^1.5) - (F)
     ]
 
     return eqs
 end
 
-T = (0:Δt:Δt*99)
-X = zeros(100,3)
-X[1,2] = F/d
+T = (0:Δt:(Δt * 99))
+X = zeros(100, 3)
+X[1, 2] = F / d
 
-prob = NonlinearProblem(f, X[1,:], (X[1,:],X[1,:]))
+prob = NonlinearProblem(f, X[1, :], (X[1, :], X[1, :]))
 sol = solve(prob, NewtonRaphson())
 
-for i=2:100
-    prob′ = remake(prob; u0=X[i-1,:], p=(X[i-1,:],X[i>2 ? i-2 : i-1, :]))
-    sol = solve(prob′, NewtonRaphson(); abstol=tol)
-    X[i,:] = sol[:]
+for i in 2:100
+    prob′ = remake(prob; u0 = X[i - 1, :], p = (X[i - 1, :], X[i > 2 ? i - 2 : i - 1, :]))
+    sol = solve(prob′, NewtonRaphson(); abstol = tol)
+    X[i, :] = sol[:]
 end
-plot(X[:,1]; ylabel="x [m]", xlabel="time step")
-plot(X[:,2]; ylabel="ẋ [m/s]", xlabel="time step")
+plot(X[:, 1]; ylabel = "x [m]", xlabel = "time step")
+plot(X[:, 2]; ylabel = "ẋ [m/s]", xlabel = "time step")
 
-plot(T, X[:,3]; ylabel="ẍ [m/s²]", xlabel="time [s]")
+plot(T, X[:, 3]; ylabel = "ẍ [m/s²]", xlabel = "time [s]")
 plot!(t_sol, ẍ_sol)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 xo = zeros(11)
 ẋo = zeros(11)
-ẋo[1] = F/d
-function du_dt3(u,p,t)
+ẋo[1] = F / d
+function du_dt3(u, p, t)
     F, k, d, Δt = p
     x, ẋ, ẍ = u
-    
-    i = round(Int, t/Δt)+1
+
+    i = round(Int, t / Δt) + 1
     xo[i] = ForwardDiff.value(x)
     ẋo[i] = ForwardDiff.value(ẋ)
 
     eqs = [
-        (ẋ) - (x-xo[i>1 ? i-1 : i])/Δt                       # D(x) = ẋ
-        (ẍ) - (ẋ-ẋo[i>1 ? i-1 : i])/Δt                       # D(ẋ) = ẍ
-        (d*ẋ + k*x^1.5) - (F)   #    0 = ( lhs ) - ( rhs )
+        (ẋ) - (x - xo[i > 1 ? i - 1 : i]) / Δt                       # D(x) = ẋ
+        (ẍ) - (ẋ - ẋo[i > 1 ? i - 1 : i]) / Δt                       # D(ẋ) = ẍ
+        (d * ẋ + k * x^1.5) - (F)   #    0 = ( lhs ) - ( rhs )
     ]
 
 
     return eqs
 end
 
-fmm = ODEFunction(du_dt3; mass_matrix=[0 0 0;0 0 0;0 0 0])
-prob = ODEProblem(fmm, [0.0, F/d, 0.0], (0.0, 0.01), [F, k, d, Δt])
-sol′′ = solve(prob, ImplicitEuler(nlsolve=NLNewton(always_new=true, check_div=false, relax=4//10)); abstol=tol, adaptive=false, dt=Δt, initializealg=NoInit()) 
+fmm = ODEFunction(du_dt3; mass_matrix = [0 0 0;0 0 0;0 0 0])
+prob = ODEProblem(fmm, [0.0, F / d, 0.0], (0.0, 0.01), [F, k, d, Δt])
+sol′′ = solve(prob, ImplicitEuler(nlsolve = NLNewton(always_new = true, check_div = false, relax = 4 // 10)); abstol = tol, adaptive = false, dt = Δt, initializealg = NoInit())
 #
-plot(sol′′ ; idxs=3, xlabel="time [s]", ylabel="ẍ [m/s^2]")
+plot(sol′′; idxs = 3, xlabel = "time [s]", ylabel = "ẍ [m/s^2]")
 
 
-
-fe(x,p,t) = error_tracker(f, x, p, t)
+fe(x, p, t) = error_tracker(f, x, p, t)
 
 function step_julia(xₒ, t)
 
     x = [xₒ]
     p = [Δt, d, k, xₒ]
     t += Δt
-    g(x) = fe(x,p,t)
+    g(x) = fe(x, p, t)
 
     while norm(g(x)) > tol
-        x = x .- ForwardDiff.jacobian(g, x)\g(x)
+        x = x .- ForwardDiff.jacobian(g, x) \ g(x)
     end
 
     return x[1], t
@@ -396,39 +349,37 @@ end
 function solver_julia()
     xs = zeros(10)
     t = 0.0
-    for i=2:10
-        xs[i], t = step_julia(xs[i-1], t)
+    for i in 2:10
+        xs[i], t = step_julia(xs[i - 1], t)
     end
     return xs
 end
 
-@time x_julia=solver_julia();
+@time x_julia = solver_julia();
 errors_julia = copy(errors)
 
 plot(x_julia)
 
-plot(iteration_error(errors_julia); yscale=:log10)
+plot(iteration_error(errors_julia); yscale = :log10)
 hline!([tol])
 
 plot(iteration_number(errors_julia))
 
 
-
-
 # DifferentialEquations.jl
 using DifferentialEquations
 
-xₒ=0.0
+xₒ = 0.0
 p = [Δt, d, k, xₒ]
 prob = SteadyStateProblem(fe, [xₒ], p)
-sol = solve(prob, NewtonRaphson(); abstol=tol)
+sol = solve(prob, NewtonRaphson(); abstol = tol)
 
 function step_diffeq(xₒ, t)
-    
-    prob′ = remake(prob, p=[Δt, d, k, xₒ], u0=[xₒ])
-    sol = solve(prob′, NewtonRaphson(); abstol=tol)
 
-    for (i,er) in enumerate(errors)
+    prob′ = remake(prob, p = [Δt, d, k, xₒ], u0 = [xₒ])
+    sol = solve(prob′, NewtonRaphson(); abstol = tol)
+
+    for (i, er) in enumerate(errors)
         if isinf(er[1])
             errors[i] = (t, er[2], er[3])
         end
@@ -441,36 +392,36 @@ empty!(errors)
 function solver_diffeq()
     xs = zeros(10)
     t = 0.0
-    for i=2:10
+    for i in 2:10
         t += Δt
-        xs[i] = step_diffeq(xs[i-1], t)
+        xs[i] = step_diffeq(xs[i - 1], t)
     end
     return xs
 end
 
-@time x_diffeq=solver_diffeq();
+@time x_diffeq = solver_diffeq();
 plot(x_diffeq)
 plot!(x_julia)
 
 errors_diffeq = copy(errors)
 
-plot(iteration_error(errors_julia); yscale=:log10)
-plot!(iteration_error(errors_diffeq); yscale=:log10)
+plot(iteration_error(errors_julia); yscale = :log10)
+plot!(iteration_error(errors_diffeq); yscale = :log10)
 hline!([tol])
 
 plot(iteration_number(errors_julia))
 plot!(iteration_number(errors_diffeq))
 
-fode = ODEFunction(fe, mass_matrix=zeros(1,1))
+fode = ODEFunction(fe, mass_matrix = zeros(1, 1))
 prob = ODEProblem(fode, [0.0], (0, Inf), [Δt, d, k, xₒ])
 empty!(errors)
-integrator = init(prob, ImplicitEuler(nlsolve=NLNewton(always_new=false, max_iter=100)); adaptive=false, dt=Δt, abstol=tol, initializealg=NoInit())
+integrator = init(prob, ImplicitEuler(nlsolve = NLNewton(always_new = false, max_iter = 100)); adaptive = false, dt = Δt, abstol = tol, initializealg = NoInit())
 
 function solver_ie()
     xs = zeros(10)
-    
-    for i=2:10
-        integrator.p[end] = xs[i-1]
+
+    for i in 2:10
+        integrator.p[end] = xs[i - 1]
         step!(integrator)
         xs[i] = integrator.u[1]
     end
@@ -485,9 +436,9 @@ plot(x_diffeq)
 plot!(x_julia)
 plot!(x_ie)
 
-plot(iteration_error(errors_julia); yscale=:log10)
-plot!(iteration_error(errors_diffeq); yscale=:log10)
-plot!(iteration_error(errors_ie); yscale=:log10)
+plot(iteration_error(errors_julia); yscale = :log10)
+plot!(iteration_error(errors_diffeq); yscale = :log10)
+plot!(iteration_error(errors_ie); yscale = :log10)
 hline!([tol])
 
 plot(iteration_number(errors_julia))
@@ -495,21 +446,20 @@ plot!(iteration_number(errors_diffeq))
 plot!(iteration_number(errors_ie))
 
 
-
 using ModelingToolkit
 using DAE2AE
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
-vars = @variables x(t)=0
+vars = @variables x(t) = 0
 
-eqs =[
-    D(x) ~ (F - k*x^1.5)/d
+eqs = [
+    D(x) ~ (F - k * x^1.5) / d,
 ]
 
 @mtkbuild odesys = ODESystem(eqs, t, vars, [])
 
 aesys = DAE2AE.dae_to_ae(odesys, Δt)
 sys = structural_simplify(aesys)
-prob = ODEProblem(sys,[], (0, Inf), [])
-integrator = init(prob, ImplicitEuler(); adaptive=false, dt=Δt, abstol=tol, initializealg=NoInit())
+prob = ODEProblem(sys, [], (0, Inf), [])
+integrator = init(prob, ImplicitEuler(); adaptive = false, dt = Δt, abstol = tol, initializealg = NoInit())
 step!(integrator)
--- dev/lectures/lecture2.jl
+++ dev/lectures/lecture2.jl
@@ -12,16 +12,16 @@ pars = @parameters begin
     x₀ = 1.0 #m
     M = 10_000 #kg
     g = 9.807 #m/s²
-    amp = 5e-2 #m
-    f = 15 #Hz    
+    amp = 5.0e-2 #m
+    f = 15 #Hz
 end
 
-dt = 1e-4 #s
+dt = 1.0e-4 #s
 t_end = 0.2 #s
 time = 0:dt:t_end
 
-x_fun(t,amp,f) = amp*sin(2π*t*f) + x₀
-ẋ_fun = build_function(expand_derivatives(D(x_fun(t,amp,f))), t, amp, f; expression=false)
+x_fun(t, amp, f) = amp * sin(2π * t * f) + x₀
+ẋ_fun = build_function(expand_derivatives(D(x_fun(t, amp, f))), t, amp, f; expression = false)
 
 
 vars = @variables begin
@@ -32,19 +32,19 @@ vars = @variables begin
     ṁ(t)
     r(t)
     ṙ(t)
-end 
+end
 
-function get_base_equations(density_type) 
-    
+function get_base_equations(density_type)
+
     eqs = [
-        D(x) ~ ẋ 
+        D(x) ~ ẋ
         D(ẋ) ~ ẍ
         D(r) ~ ṙ
 
-        r ~ r₀*(1 + p/β)
+        r ~ r₀ * (1 + p / β)
 
-        ṁ ~ ṙ*x*A + (density_type)*ẋ*A
-        M*ẍ ~ p*A - M*g
+        ṁ ~ ṙ * x * A + (density_type) * ẋ * A
+        M * ẍ ~ p * A - M * g
     ]
 
     return eqs
@@ -52,55 +52,55 @@ end
 
 eqs_ṁ1 = [
     get_base_equations(r₀)...
-    ṁ ~ ẋ_fun(t,amp,f)*A*r
+    ṁ ~ ẋ_fun(t, amp, f) * A * r
 ]
 
 eqs_ṁ2 = [
     get_base_equations(r)...
-    ṁ ~ ẋ_fun(t,amp,f)*A*r
+    ṁ ~ ẋ_fun(t, amp, f) * A * r
 ]
 
 eqs_x = [
     get_base_equations(r)...
-    x ~ x_fun(t,amp,f)
+    x ~ x_fun(t, amp, f)
 ]
 
 @mtkbuild odesys_x = ODESystem(eqs_x, t, vars, pars)
 prob_x = ODEProblem(sys_x, [], (0, t_end))
-sol_x = solve(prob_x; saveat=time)
+sol_x = solve(prob_x; saveat = time)
 
 @mtkbuild odesys_ṁ1 = ODESystem(eqs_ṁ1, t, vars, pars)
 u0 = [sol_x[s][1] for s in unknowns(sys_ṁ1)]
 prob_ṁ1 = ODEProblem(sys_ṁ1, u0, (0, t_end))
 sol_ṁ1 = solve(prob_ṁ1)
 
-plot(sol_ṁ1; idxs=ṁ, label="guess", ylabel="ṁ")
-plot!(sol_x; idxs=ṁ, label="solution")
+plot(sol_ṁ1; idxs = ṁ, label = "guess", ylabel = "ṁ")
+plot!(sol_x; idxs = ṁ, label = "solution")
 
 @mtkbuild odesys_ṁ2 = ODESystem(eqs_ṁ2, t, vars, pars)
 prob_ṁ2 = ODEProblem(sys_ṁ2, u0, (0, t_end))
 sol_ṁ2 = solve(prob_ṁ2)
 
-plot(sol_x; idxs=x, label="solution", ylabel="x")
-plot!(sol_ṁ1; idxs=x, label="case 1")
-plot!(sol_ṁ2; idxs=x, label="case 2")
+plot(sol_x; idxs = x, label = "solution", ylabel = "x")
+plot!(sol_ṁ1; idxs = x, label = "case 1")
+plot!(sol_ṁ2; idxs = x, label = "case 2")
 
-plot(time, (sol_ṁ1(time)[x] .- sol_ṁ2(time)[x])/1e-3, ylabel="x error [mm]", xlabel="t [s]")
+plot(time, (sol_ṁ1(time)[x] .- sol_ṁ2(time)[x]) / 1.0e-3, ylabel = "x error [mm]", xlabel = "t [s]")
 
 # -----------------------------------------------------
 
 using DataInterpolations
 mass_flow_fun = LinearInterpolation(sol_x[ṁ], sol_x.t)
 
-open("dm.jl","w") do io
+open("dm.jl", "w") do io
     print(io, "u = [")
     join(io, sol_x[ṁ], ',')
     print(io, "]")
 end
 
 
-plot(sol_x; idxs=ṁ)
-plot!(time, -263.9489641054512*cos.(2π*time*15))
+plot(sol_x; idxs = ṁ)
+plot!(time, -263.9489641054512 * cos.(2π * time * 15))
 
 import ModelingToolkitStandardLibrary.Mechanical.Translational as T
 import ModelingToolkitStandardLibrary.Hydraulic.IsothermalCompressible as IC
@@ -113,20 +113,20 @@ function MassVolume(; name, dx, drho, dm)
         x₀ = 1.0 #m
         M = 10_000 #kg
         g = 9.807 #m/s²
-        amp = 5e-2 #m
-        f = 15 #Hz   
-        p_int=M*g/A
-        dx=dx
-        drho=drho
-        dm=dm
+        amp = 5.0e-2 #m
+        f = 15 #Hz
+        p_int = M * g / A
+        dx = dx
+        drho = drho
+        dm = dm
     end
     vars = []
     systems = @named begin
         fluid = IC.HydraulicFluid(; density = 876, bulk_modulus = 1.2e9)
-        mass = T.Mass(;v=dx,m=M,g=-g)
-        vol = IC.Volume(;area=A, x=x₀, p=p_int, dx, drho, dm)
-        mass_flow = IC.MassFlow(;p_int)
-        mass_flow_input = B.TimeVaryingFunction(;f = mass_flow_fun)
+        mass = T.Mass(; v = dx, m = M, g = -g)
+        vol = IC.Volume(; area = A, x = x₀, p = p_int, dx, drho, dm)
+        mass_flow = IC.MassFlow(; p_int)
+        mass_flow_input = B.TimeVaryingFunction(; f = mass_flow_fun)
     end
 
     eqs = [
@@ -148,13 +148,12 @@ dm = sol_x[ṁ][1]
 sys = structural_simplify(odesys)
 # sys = no_simplify(expand_connections(odesys)) |> complete
 prob = ODEProblem(sys, [], (0, t_end))
-sol=solve(prob)
-plot(sol; idxs=sys.vol.x, linewidth=2)
-plot!(sol_x; idxs=x)
+sol = solve(prob)
+plot(sol; idxs = sys.vol.x, linewidth = 2)
+plot!(sol_x; idxs = x)
 
-plot(sol; idxs=sys.vol.dx, linewidth=2)
-plot!(sol_x; idxs=ẋ)
-
+plot(sol; idxs = sys.vol.dx, linewidth = 2)
+plot!(sol_x; idxs = ẋ)
 
 
 du0 = [
@@ -162,22 +161,22 @@ du0 = [
     sol_x[ẋ][1] # vol₊x(t)
     sol_x[ẋ][1] # vol₊moving_volume₊x(t)
     sol_x[ṙ][1] # vol₊moving_volume₊rho(t)
-    sol_x[ṙ*β/r₀][1] # vol₊moving_volume₊port₊p(t)
-    sol_x[ṙ*β/r₀][1] # vol₊damper₊port_b₊p(t)
+    sol_x[ṙ * β / r₀][1] # vol₊moving_volume₊port₊p(t)
+    sol_x[ṙ * β / r₀][1] # vol₊damper₊port_b₊p(t)
     0 # vol₊moving_volume₊drho(t)
 ]
 prob = DAEProblem(sys, du0, [], (0, t_end))
-sol = solve(prob, DImplicitEuler(nlsolve=NLNewton(check_div=false)))
+sol = solve(prob, DImplicitEuler(nlsolve = NLNewton(check_div = false)))
 
 prob = ODEProblem(sys, [], (0, t_end))
-sol = solve(prob, ImplicitEuler(nlsolve=NLNewton(check_div=false, max_iter=100, relax=4//10)); dt, adaptive=false)
+sol = solve(prob, ImplicitEuler(nlsolve = NLNewton(check_div = false, max_iter = 100, relax = 4 // 10)); dt, adaptive = false)
 
 using SimpleEuler
-sol = solve(prob, BackwardEuler(); dt, adaptive=false)
+sol = solve(prob, BackwardEuler(); dt, adaptive = false)
 
-plot(sol; idxs=sys.vol.x, linewidth=2)
-plot!(sol_x; idxs=x)
+plot(sol; idxs = sys.vol.x, linewidth = 2)
+plot!(sol_x; idxs = x)
 
-plot(sol; idxs=sys.mass_flow.dm.u)
+plot(sol; idxs = sys.mass_flow.dm.u)
 
-plot(sol; idxs=sys.vol.moving_volume.port.p)
+plot(sol; idxs = sys.vol.moving_volume.port.p)
